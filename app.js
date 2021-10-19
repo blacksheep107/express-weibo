c925dfab4fc72fadbdf942629064bb39a4bf8702
blacksheep107 <blacksheep107@126.com>
2021-10-19 17:43:04 +0800

    getTen

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/weibo/test', testRouter);
+var getTenPosts = require('./routes/weibo/getTenPosts');
+app.use('/weibo/getTenPosts', getTenPosts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
