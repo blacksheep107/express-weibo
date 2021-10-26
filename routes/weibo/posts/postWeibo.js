661ce0206ce13b9dca64a522085b017479f73cac
blacksheep107 <blacksheep107@126.com>
2021-10-26 10:45:39 +0800

    post

--- routes/weibo/posts/postWeibo.js
+++ routes/weibo/posts/postWeibo.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var router = express.Router();
 const { MongoClient, ObjectId} = require('mongodb');
-const uri = require('../../common/common.json').url;
+const uri = require('../../../common/common.json').url;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 router.post('/', function(req, res, next) {
     try {
         let postData = req.body;
+        console.log(ObjectId(postData['post_user']))
         client.connect(err => {
             const users = client.db('weibo').collection('users');
             users.find({
                 _id: ObjectId(postData['post_user'])
             }).skip(0).limit(1).toArray(function (err, result) {
+                console.log(result)
                 if (err) throw err;
                 if (result.length === 0) {
                     res.send({
